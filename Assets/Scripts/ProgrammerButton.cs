d8f4142aa1b58835813c3e0f8ded527d9a724f49
depperm <epper.marshall@gmail.com>
2018-03-19 22:10:44 -0400

    some scripts

--- Assets/Scripts/ProgrammerButton.cs
+++ Assets/Scripts/ProgrammerButton.cs
@@ -1,14 +1,44 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine.UI;
 
 public class ProgrammerButton : MonoBehaviour {
+	public RectTransform programmerComponent;
+	public Text nameLabel;
+	public Image identiconImage;
+	public Image backgroundImage;
+	public Text progressText;
+	public Text numberHiredText;
 
+	public Button singleHire;
+	public Text singleCostText;
+	public Text rateText;
+
+	public Button maxHire;
+	public Text numberToHireText;
+	public Text maxCostText;
+
+	public Button autoHire;
+	public Text autoCostText;
+
+	//private Item item;
+	//private ProgrammerScrollList scrollList;
+	//public Text priceText;
 	// Use this for initialization
 	void Start () {
+		//buttonComponent.onClick.AddListener (HandleClick);
+	}
+
+	public void Setup(){
 		
 	}
-	
+
+	public void HandleClick()
+	{
+		//scrollList.TryTransferItemToOtherShop (item);
+	}
+
 	// Update is called once per frame
 	void Update () {
 		
